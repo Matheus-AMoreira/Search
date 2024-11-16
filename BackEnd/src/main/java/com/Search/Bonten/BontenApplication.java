153f5b6f4c3ca0e1b22b75b385c33426b3053800
Matheus-AMoreira <matheus.moreira2172@gmail.com>
2024-11-16 01:15:47 -0300

    First Setup

---
+++ BackEnd/src/main/java/com/Search/Bonten/BontenApplication.java
@@ -0,0 +1,13 @@
+package com.Search.Bonten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BontenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BontenApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/Search/Bonten/BontenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Search.Bonten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BontenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
